a010949f8935e30c32538bf4b9e95db1fd6dda6b
andresafonov95-ux <andre.safonov95@gmail.com>
2026-03-04 01:09:40 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@ import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, FSInputFile
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 
 TOKEN = os.getenv("TOKEN")
 
@@ -32,6 +34,16 @@ async def send_video(callback: types.CallbackQuery):
 
 async def main():
     await dp.start_polling(bot)
+
+async def handle_video(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    video = update.message.video
+    print("VIDEO FILE_ID:", video.file_id)
+
+app = ApplicationBuilder().token("YOUR_BOT_TOKEN").build()
+
+app.add_handler(MessageHandler(filters.VIDEO, handle_video))
+
+app.run_polling()
 
 if __name__ == "__main__":
     asyncio.run(main())
